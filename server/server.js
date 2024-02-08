bc4011991d8704a6a22968552472150dc25ceafe
kuldeep8454 <maddeshiyakuldeep@gmail.com>
2024-02-08 11:56:59 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const app = express();
 app.use(express.json());
-require('dotenv').config();
+require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
-const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const usersRoute = require('./routes/usersRoute');
+const usersRoute = require("./routes/usersRoute");
 const productsRoute = require("./routes/productsRoute");
 const bidsRoute = require("./routes/bidsRoute");
 const notificationsRoute = require('./routes/notificationsRoute');
 
+// Use the cors middleware to allow requests from all origins
 app.use(cors());
 
-app.use('/api/users', usersRoute);
+app.use("/api/users", usersRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/bids", bidsRoute);
-app.use("/api/notifications", notificationsRoute);
-
+app.use('/api/notifications', notificationsRoute);
 
 // deployment config
 const path = require("path");
@@ -30,4 +30,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => console.log(`Nodemon server started : ${port}`));
+
+app.listen(port, () =>
+  console.log(`Node/Express Server started on port ${port}`)
+);
